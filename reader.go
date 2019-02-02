77f78a362cf03c676234f56dff38cbb6bb40d04f
xeoncross <david@xeoncross.com>
2019-02-01 18:23:43 -0600

    New, handler callback design for reader

---
+++ reader.go
@@ -0,0 +1,142 @@
+package mimestream
+
+import (
+	"bufio"
+	"encoding/base64"
+	"io"
+	"mime"
+	"mime/multipart"
+	"net/textproto"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// Most emails will never contain more than 3 levels of nested multipart bodies
+var MaximumMultipartDepth = 10
+
+var ErrMaximumMultipartDepth = errors.New("Mimestream: Maximum multipart/mime nesting level reached")
+
+// Most emails will never have more than a dozen attachments / text parts total
+var MaximumPartsPerMultipart = 50
+
+var ErrMaximumPartsPerMultipart = errors.New("Mimestream: Maximum number of multipart/mime parts reached")
+
+// ErrMissingBoundary for multipart bodies without a boundary header
+var ErrMissingBoundary = errors.New("Missing boundary")
+
+// The format of the callback for handling MIME emails
+type partHandler func(textproto.MIMEHeader, io.Reader) error
+
+// NewEmailFromReader reads a stream of bytes from an io.Reader, r,
+// and returns an email struct containing the parsed data.
+// This function expects the data in RFC 5322 format.
+func HandleEmailFromReader(r io.Reader, h partHandler) (err error) {
+	tp := textproto.NewReader(bufioReader(r))
+
+	var header textproto.MIMEHeader
+	header, err = tp.ReadMIMEHeader()
+	if err != nil {
+		return
+	}
+
+	// FYI: http.Header and textproto.MIMEHeader are both map[string][]string
+	// http.Header(header)
+	// header.(map[string][]string)
+	// (*map[string][]string).(header)
+
+	// Recursively parse the MIME parts
+	err = parseMIMEParts(header, tp.R, h, 0)
+	return
+}
+
+// parseMIMEParts will recursively walk a MIME entity calling the handler
+func parseMIMEParts(hs textproto.MIMEHeader, body io.Reader, handler partHandler, level int) (err error) {
+
+	// Protect against bad actors
+	if level > MaximumMultipartDepth {
+		return ErrMaximumMultipartDepth
+	}
+
+	ct, params, err := mime.ParseMediaType(hs.Get("Content-Type"))
+	if err != nil {
+		return
+	}
+
+	// Either a leaf node, or not a multipart email
+	if !strings.HasPrefix(ct, "multipart/") {
+		handler(hs, contentDecoderReader(hs, body))
+		return
+	}
+
+	// Should we allow this?
+	if _, ok := params["boundary"]; !ok {
+		return ErrMissingBoundary
+	}
+
+	// Readers are buffered https://golang.org/src/mime/multipart/multipart.go#L99
+	mr := multipart.NewReader(body, params["boundary"])
+
+	var partsCounter int
+	var p *multipart.Part
+	for {
+
+		// Decodes quotedprintable: https://golang.org/src/mime/multipart/multipart.go#L128
+		// Closes last part reader: https://golang.org/src/mime/multipart/multipart.go#L302
+		p, err = mr.NextPart()
+		if err == io.EOF {
+			err = nil
+			break
+		}
+
+		if err != nil {
+			return
+		}
+
+		// Protect against bad actors
+		partsCounter++
+		if partsCounter > MaximumPartsPerMultipart {
+			return ErrMaximumPartsPerMultipart
+		}
+
+		// Correctly decode the body bytes
+		body := contentDecoderReader(p.Header, p)
+
+		var subct string
+		subct, _, err = mime.ParseMediaType(p.Header.Get("Content-Type"))
+
+		// Nested multipart
+		if strings.HasPrefix(subct, "multipart/") {
+			err = parseMIMEParts(p.Header, body, handler, level+1)
+			if err != nil {
+				return
+			}
+
+		} else {
+			// Leaf node
+			handler(p.Header, body)
+		}
+	}
+
+	return
+}
+
+// contentDecoderReader
+func contentDecoderReader(headers textproto.MIMEHeader, bodyReader io.Reader) *bufio.Reader {
+	// Already handled by textproto
+	// if headers.Get("Content-Transfer-Encoding") == "quoted-printable" {
+	// 	return bufioReader(quotedprintable.NewReader(bodyReader))
+	// }
+	if headers.Get("Content-Transfer-Encoding") == "base64" {
+		return bufioReader(base64.NewDecoder(base64.StdEncoding, bodyReader))
+	}
+	return bufioReader(bodyReader)
+}
+
+// bufioReader ...
+func bufioReader(r io.Reader) *bufio.Reader {
+	if bufferedReader, ok := r.(*bufio.Reader); ok {
+		return bufferedReader
+	}
+	return bufio.NewReader(r)
+}
---
+++ reader_test.go
@@ -0,0 +1,124 @@
+package mimestream
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/textproto"
+	"runtime"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestReader(t *testing.T) {
+
+	var err error
+
+	parts := Parts{
+		Part{
+			ContentType: TextPlain,
+			Source: TextPart{
+				Text: "Hello World",
+			},
+		},
+		Part{
+			ContentType: "image/jpeg",
+			Source: File{
+				Name:   "filename.jpg",
+				Reader: mockDataSrc(32), // in bytes
+			},
+		},
+		// Part{
+		// 	ContentType: TextPlain,
+		// 	Source: File{
+		// 		Name:   "filename-2 שלום.txt",
+		// 		Inline: true,
+		// 		Reader: tmpfile,
+		// 		Closer: tmpfile,
+		// 	},
+		// },
+		Part{
+			ContentType: "application/json",
+			Source: File{
+				Name:   "payload.json",
+				Reader: strings.NewReader(`{"one":1,"two":2}`),
+			},
+		},
+	}
+
+	// To pipe to reader
+	pr, pw := io.Pipe()
+
+	// Log everything we read
+	// sow := &StdoutWriter{}
+
+	// Log how much data passed through
+	wc := &WriteCounter{}
+
+	// One write to rule-them-all
+	forkedwriter := io.MultiWriter(wc, pw) //, sow)
+
+	// Report
+	go func() {
+		for {
+			select {
+			case <-time.After(time.Second):
+				var m runtime.MemStats
+				runtime.ReadMemStats(&m)
+				fmt.Printf("Encoded and sent %10d MB/s (%10d MB Total) using %7d MB of RAM\n", wc.Recent()/1024/1024, wc.Total()/1024/1024, m.Alloc/1024/1024)
+			}
+		}
+	}()
+
+	mw := multipart.NewWriter(forkedwriter)
+
+	// writing without a reader will deadlock so write in a goroutine
+	go func() {
+		// Start the pipeline
+		err = parts.Into(mw)
+		if err != nil {
+			t.Error(err)
+		}
+
+		pw.Close()
+	}()
+
+	// ioutil.ReadAll(pr)
+
+	headers := strings.Join([]string{
+		"From: John <john@example.com>",
+		"Mime-Version: 1.0 (1.0)",
+		"Date: Thu, 10 Jan 2002 11:12:00 -0700",
+		"Subject: My Temp Message",
+		"Message-Id: <1234567890>",
+		"To: <user@example.com>",
+		"Content-Type: " + mw.FormDataContentType()}, "\r\n") + "\r\n\r\n"
+
+	mailreader := io.MultiReader(strings.NewReader(headers), pr)
+
+	var partCounter int
+
+	err = HandleEmailFromReader(mailreader, func(header textproto.MIMEHeader, body io.Reader) (err error) {
+		partCounter++
+
+		b, err := ioutil.ReadAll(body)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Part Header: %v\n", header)
+		fmt.Printf("Part body: %q\n", b)
+
+		return
+	})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if partCounter != 3 {
+		t.Error("Invalid number of parts found")
+	}
+}
